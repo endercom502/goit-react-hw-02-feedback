bd624099508ec796491451059352b488b7c00455
Andrzej Markiewicz <endercom502@gmail.com>
2022-11-05 22:21:28 +0100

    gd

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,1 +1,23 @@
-import style from "./Statistics.module.css";
+import style from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+    return (
+    <>
+        <div className={style.good}>Good: {good}</div>
+        <div className={style.neutral}>Neutral: {neutral}</div>
+        <div className={style.bad}>Bad: {bad}</div>
+        <div className={style.option}>Total: {total}</div>
+        <div className={style.positive}>Positive Feedback: {positivePercentage}%</div>
+    </>
+        
+    )
+}
+
+Statistics.prototype = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
